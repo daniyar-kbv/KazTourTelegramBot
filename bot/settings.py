d0a7aaabd0d315ef5027897833e2ffec058cf487
daniyarka <daniyar.kbv@gmail.com>
2021-05-19 20:06:35 +0600

    testing

--- bot/settings.py
+++ bot/settings.py
@@ -7,10 +7,10 @@ import constants
 bot = telebot.TeleBot(os.environ.get('TELEGRAM_BOT_TOKEN'))
 
 
-# if settings.DEBUG:
-#     start_bot.delay()
-# else:
-try:
-    bot.set_webhook(url=constants.TELEGRAM_BOT_URL)
-except:
-    pass
+if settings.DEBUG:
+    start_bot.delay()
+else:
+    try:
+        bot.set_webhook(url=constants.TELEGRAM_BOT_URL)
+    except:
+        pass
