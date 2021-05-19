57fa58b4cb71802d46785d802333ce2a38a6e959
daniyarka <daniyar.kbv@gmail.com>
2021-05-19 18:51:18 +0600

    testing

--- kaztour_bot/urls.py
+++ kaztour_bot/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('main/', include('main.urls'))
+    path('bot_test/admin/', admin.site.urls),
+    path('bot_test/main/', include('main.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
     static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
